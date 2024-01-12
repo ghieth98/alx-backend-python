f3714019b115c64db80855fbe276f5f3ad3ebbe0
hossam ghieth <hossamghieth@gmail.com>
2024-01-12 02:29:01 +0200

    Update 3-to_str.py

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 """
-function to_str that takes a float n as argument and returns the string representation of the float.
+function to_str that takes a float n as argument
+ and returns the string representation of the float.
 """
 
 
