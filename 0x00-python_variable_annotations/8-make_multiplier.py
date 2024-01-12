764173b30666b7db9c6fc063479f5cc374a4cf95
hossam ghieth <hossamghieth@gmail.com>
2024-01-12 02:35:25 +0200

    Update 8-make_multiplier.py

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
 """
-function make_multiplier that takes a float multiplier as argument
- and returns a function that multiplies a float by multiplier.
+function make_multiplier that
+ takes a float multiplier as argument
+ and returns a function that multiplies
+a float by multiplier.
 """
 from typing import Callable
 
