8930ab610df4ee285ef497a39cd272ad7d340fb0
hossam ghieth <hossamghieth@gmail.com>
2024-01-12 02:23:25 +0200

    Create 2-floor.py

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+function that takes float n argument and returns the floor of the float
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Function that takes float n argument and returns the floor of the float
+    :param n:
+    :return: int
+    """
+    return math.floor(n)
