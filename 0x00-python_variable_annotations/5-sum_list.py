e9cd15de41e60c80f7f85a5357297004b6bcd2b0
hossam ghieth <hossamghieth@gmail.com>
2024-01-12 02:32:28 +0200

    Update 5-sum_list.py

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 """
-function sum_list which takes a list input_list of floats as argument and returns their sum as a float.
+function sum_list which takes a list
+ input_list of floats as argument
+ and returns their sum as a float.
 """
 from typing import List
 
