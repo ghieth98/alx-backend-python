d4b89460c0c17d27eb51acf3af999bc199d88cae
hossam ghieth <hossamghieth@gmail.com>
2024-01-12 02:23:29 +0200

    Create 3-to_str.py

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+function to_str that takes a float n as argument and returns the string representation of the float.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    takes a float n as argument and returns the string representation of the result
+    :param n:
+    :return: str
+    """
+    return str(n)
