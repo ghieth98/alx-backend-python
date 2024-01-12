086a36685e688c0ffd56f30bb75517aefa9017ac
hossam ghieth <hossamghieth@gmail.com>
2024-01-12 02:23:20 +0200

    Create 0-add.py

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Function to return the sum of two numbers as float
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Function to return the sum of two numbers as float
+    :param a:
+    :param b:
+    :return: float
+    """
+    return a + b
