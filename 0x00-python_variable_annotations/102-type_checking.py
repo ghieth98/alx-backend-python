84944854dd106c813a85a7cabc65fcfdfe925fa6
hossam ghieth <hossamghieth@gmail.com>
2024-01-12 02:23:57 +0200

    Create 102-type_checking.py

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Use mypy to validate the following piece of code and apply any necessary changes.
+"""
+from typing import Tuple, List
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """
+
+    :param lst:
+    :param factor:
+    :return:
+    """
+    zoomed_in: List = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array = tuple([12, 72, 91])
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, int(3.0))
