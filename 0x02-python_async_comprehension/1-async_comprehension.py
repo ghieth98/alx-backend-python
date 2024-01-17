865ed62dbd20eb3e78dc6d2b3721eefab11de442
hossam ghieth <hossamghieth@gmail.com>
2024-01-17 02:30:06 +0200

    Create 1-async_comprehension.py

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Module 1-async_comprehension.py """
+from typing import List
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """
+    Collect 10 random numbers using an async comprehensive over
+    async_generator, then return the 10 random numbers.
+    """
+    return [i async for i in async_generator()]
